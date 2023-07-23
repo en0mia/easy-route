fdf5e1a3cd128844d25305ed831235653f3346dd
Simone Nicol <en0mia.dev@gmail.com>
2023-07-23 12:49:57 +0200

    :sparkles: Implement example tests to test the workflow.

---
+++ tests/example/test_example.py
@@ -0,0 +1,8 @@
+# @author Simone Nicol <en0mia.dev@gmail.com>
+# @created 23/07/23
+import unittest
+
+
+class TestExample(unittest.TestCase):
+    def testExample(self):
+        self.assertEqual(True, True)
