1dfefa62f6033fbfa7e20f3c51d242d6408a60b4
Simone Nicol <en0mia.dev@gmail.com>
2023-07-24 14:39:25 +0200

    Features/implement middlewares (#2)
    * :sparkles: Implement DataValidatorMiddleware

---
+++ src/easy_route_en0mia/middlewares/data_validator_middleware.py
@@ -0,0 +1,31 @@
+# @author Simone Nicol <en0mia.dev@gmail.com>
+# @created 23/07/23
+from typing import Optional
+
+from flask import Request, Response
+
+from easy_route_en0mia.middlewares.abstract_middleware import AbstractMiddleware
+
+
+class DataValidatorMiddleware(AbstractMiddleware):
+    """Validates the JSON input using a rule mapping."""
+    def __init__(self, rules):
+        """
+        :param rules: A map in the form field_name => validation_method
+        """
+        self.rules = rules
+        super().__init__()
+
+    def dispatch(self, request: Request) -> Optional[Response]:
+        """Validates the JSON input by means of the validation methods in the rules map.
+        :param request: the Request to run the middleware check against
+        :return: Response | None
+        """
+        data = request.get_json(silent=True)
+        if not data:
+            return Response('', 400)
+
+        for argument, validator in self.rules.items():
+            if not data[argument] or not validator(data[argument]):
+                return Response('', 400)
+        return None
---
+++ tests/middleware/test_data_validator_middleware.py
@@ -0,0 +1,94 @@
+# @author Simone Nicol <en0mia.dev@gmail.com>
+# @created 23/07/23
+import json
+import unittest
+import uuid
+from unittest.mock import MagicMock
+
+from easy_route_en0mia.middlewares.data_validator_middleware import DataValidatorMiddleware
+
+
+class TestDataValidatorMiddleware(unittest.TestCase):
+    def setUp(self) -> None:
+        self.request = MagicMock()
+        self.validator = MagicMock()
+
+    def tearDown(self) -> None:
+        self.request = None
+        self.validator = None
+
+    def testDispatch_return400_whenDataIsEmpty(self):
+        # Arrange
+        rules = {}
+        middleware = DataValidatorMiddleware(rules)
+
+        self.request.get_json.return_value = None
+
+        # Act
+        result = middleware.dispatch(self.request)
+
+        # Assert
+        self.request.get_json.assert_called_once()
+        self.assertEqual(400, result.status_code)
+        self.assertFalse(result.data)
+
+    def testDispatch_return400_whenInvalidParameter(self):
+        # Arrange
+        rules = {
+            'uuid': self.validator.is_uuid
+        }
+        middleware = DataValidatorMiddleware(rules)
+
+        self.validator.is_uuid.return_value = False
+        self.request.get_json.return_value = {
+            'uuid': 'invalid uuid'
+        }
+
+        # Act
+        result = middleware.dispatch(self.request)
+
+        # Assert
+        self.request.get_json.assert_called_once()
+        self.assertEqual(400, result.status_code)
+        self.assertFalse(result.data)
+
+    def testDispatch_return400_whenParameterIsNotSet(self):
+        # Arrange
+        rules = {
+            'uuid': self.validator.is_uuid
+        }
+        middleware = DataValidatorMiddleware(rules)
+
+        self.validator.is_uuid.return_value = False
+        self.request.get_json.return_value = {}
+
+        # Act
+        result = middleware.dispatch(self.request)
+
+        # Assert
+        self.request.get_json.assert_called_once()
+        self.validator.is_uuid.assert_not_called()
+        self.assertEqual(400, result.status_code)
+        self.assertFalse(result.data)
+
+    def testDispatch_returnNone_whenValidParameters(self):
+        # Arrange
+        rules = {
+            'uuid': self.validator.is_uuid,
+            'name': lambda name: self.validator.is_string(name) and name,
+        }
+        middleware = DataValidatorMiddleware(rules)
+
+        self.validator.is_uuid.return_value = True
+        self.validator.is_string.return_value = True
+        self.request.get_json.return_value = {
+            'uuid': str(uuid.uuid4()),
+            'name': 'name'
+        }
+
+        # Act
+        result = middleware.dispatch(self.request)
+
+        # Assert
+        self.request.get_json.assert_called_once()
+        self.assertIsNone(result)
